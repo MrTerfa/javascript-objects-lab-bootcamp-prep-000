2469f4d141536c9ab4c27f5741a8818d3453c2f5
Terfa Tortema <terfa.tortema@gmail.com>
2017-09-14 17:07:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {
+}
+
+var updateObjectWithKeyAndValue = function(object,key,value){
+  return Object.assign({},object,{[key]:value});
+}
+
+var destructivelyUpdateObjectWithKeyAndValue = function(object,key,value){
+  object[key] = value;
+  return object;
+}
+
+var deleteFromObjectByKey = function(object,key){
+  var newObject = Object.assign({},object)
+  delete newObject[key]
+  return newObject;
+}
+
+var destructivelyDeleteFromObjectByKey(object,key){
+  return object
+}
